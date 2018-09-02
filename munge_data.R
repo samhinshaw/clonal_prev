b4a1bbe4a891aa9df0711bc69a94b0f9834e2df5
Sam Hinshaw <samuel.hinshaw@gmail.com>
2018-09-02 11:32:58 -0700

    munge state data as well

--- munge_data.R
+++ munge_data.R
@@ -10,3 +10,9 @@ tree_edges  <- read_csv(here("data", "px7_tree_edges.csv"))
 clonal_prev_spread <- clonal_prev %>%
   spread(clone_id, clonal_prev) %T>%
   write_csv(here("data", "px7_clonal_prev_spread.csv"))
+
+## gather state data
+state_data <- read_csv(here("data", "data.csv"))
+long_state_data <- state_data %>%
+  gather(key = "age_group", value = "count", -State) %T>%
+  write_csv(here("data", "long_state_data.csv"))
